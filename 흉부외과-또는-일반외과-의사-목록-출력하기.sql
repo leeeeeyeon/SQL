f38be971816b494d0f08216a1a7ebdad17ff37b4
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-07 23:18:19 +0900

    style: 주석 스타일 변경

--- 흉부외과-또는-일반외과-의사-목록-출력하기.sql
+++ 흉부외과-또는-일반외과-의사-목록-출력하기.sql
@@ -3,7 +3,7 @@ FROM DOCTOR
 WHERE MCDP_CD='CS' OR MCDP_CD='GS'
 ORDER BY HIRE_YMD DESC, DR_NAME
 
-# ASC - Default, 오름차순
-# DESC - 내림차순
-# 날짜 형식 지정 - DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
-# 기타 날짜 형식 참고 - https://velog.io/@donghoim/MySQL-DATETIME-원하는-유형으로-변경-YYMMDD
+-- ASC - Default, 오름차순
+--  DESC - 내림차순
+--  날짜 형식 지정 - DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+--  기타 날짜 형식 참고 - https://velog.io/@donghoim/MySQL-DATETIME-원하는-유형으로-변경-YYMMDD
