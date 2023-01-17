f89843e3274fef4aef787af1f1fda801e93c9d4f
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-17 14:50:53 +0900

    add: 루시와 엘라 찾기

---
+++ 루시와-엘라-찾기.sql
@@ -0,0 +1,7 @@
+-- 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
+-- 이때 결과는 아이디 순으로 조회해주세요.
+
+select animal_id, name, sex_upon_intake
+from animal_ins
+where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+order by animal_id
