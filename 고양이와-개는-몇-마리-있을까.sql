0b9621e2202a527b2bdb9345d3e6615bc08e228c
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-20 13:45:04 +0900

    comment: 다른 사람 코드 추가 및 문법 설명 추가

--- 고양이와-개는-몇-마리-있을까.sql
+++ 고양이와-개는-몇-마리-있을까.sql
@@ -6,3 +6,14 @@ FROM ANIMAL_INS
 WHERE ANIMAL_TYPE = 'CAT' OR ANIMAL_TYPE = 'DOG'
 GROUP BY ANIMAL_TYPE
 ORDER BY ANIMAL_TYPE
+
+-- 다른 사람 코드
+-- SELECT  ANIMAL_TYPE, COUNT(ANIMAL_ID) AS count
+-- FROM ANIMAL_INS
+-- WHERE ANIMAL_TYPE IN ('Cat', 'Dog')
+-- GROUP BY ANIMAL_TYPE
+-- ORDER BY FIELD(ANIMAL_TYPE, 'Cat', 'Dog')
+
+-- IN으로 고양이 또는 강아지만 출력
+-- ORDER BY FIELD(ANIMAL_TYPE, 'Cat', 'Dog'): ANIMAL_TYPE을 정렬, Cat 1순위, Dog 2순위, 나머지를 오름차순으로 정렬
+-- DESC를 붙이고 싶을 때는 닫는 괄호 뒤에
