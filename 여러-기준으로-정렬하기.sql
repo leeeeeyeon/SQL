058cbb1b7dce2513f4a792c2458adb7e0852da42
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-23 01:35:00 +0900

    add: 여러 기준으로 정렬하기

---
+++ 여러-기준으로-정렬하기.sql
@@ -0,0 +1,5 @@
+-- 동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회하는 SQL문을 작성해주세요. 단, 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 합니다.
+
+select animal_id, name, datetime
+from animal_ins
+order by name, datetime desc
