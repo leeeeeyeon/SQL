c0c3e264f5ff0a1d93377c20edccead8946d9d5a
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-23 01:35:27 +0900

    add: 입양 시각 구하기(1)

---
+++ 입양-시각-구하기(1).sql
@@ -0,0 +1,10 @@
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요.
+-- 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+select hour(datetime) as hour, count(animal_id) as count
+from animal_outs
+where hour(datetime) >= 9 and hour(datetime) <= 19
+group by hour
+order by hour
+
+-- where 절에서는 별칭을 사용하는 것이 불가능하다!
