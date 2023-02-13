970ac854ec931c6b7929b94332562cc7ce915c44
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-02-13 23:40:30 +0900

    fix: 자동차 대여 기록에서 대여중/대여 가능 여부 구분하기

--- 자동차-대여-기록에서-대여중-대여-가능-여부-구분하기.sql
+++ 자동차-대여-기록에서-대여중-대여-가능-여부-구분하기.sql
@@ -1,7 +1,7 @@
 -- CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 2022년 10월 16일에 대여 중인 자동차인 경우 '대여중' 이라고 표시하고, 대여 중이지 않은 자동차인 경우 '대여 가능'을 표시하는 컬럼(컬럼명: AVAILABILITY)을 추가하여 자동차 ID와 AVAILABILITY 리스트를 출력하는 SQL문을 작성해주세요. 
 -- 이때 반납 날짜가 2022년 10월 16일인 경우에도 '대여중'으로 표시해주시고 결과는 자동차 ID를 기준으로 내림차순 정렬해주세요.
 
-select car_id, if('2022-10-16' between start_date and end_date, 1, 0), '대여중', '대여 가능') as availability
+select car_id, if(sum(if('2022-10-16' between start_date and end_date, 1, 0)) > 0, '대여중', '대여 가능') as availability
 from car_rental_company_rental_history
 group by car_id
 order by car_id desc
